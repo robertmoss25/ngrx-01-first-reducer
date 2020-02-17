9fba851322cf665a55267ab5ccff1135ee2b8cdc
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2020-02-17 15:41:22 -0500

    Finishing the Login Effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,10 +1,11 @@
 import { Actions,ofType,Effect } from '@ngrx/effects';
 import * as AuthActions from './auth.actions';
-import { switchMap, catchError, map, reduce } from 'rxjs/operators';
+import { switchMap, catchError, map, reduce, tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { of } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData {
     kind: string;
@@ -24,7 +25,7 @@ export class AuthEffects {
         switchMap((authData: AuthActions.LoginStart) => {
             return this.http
                     .post<AuthResponseData>(
-                    'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + environment.firebaseAPIKey,
+                    'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + environment.firebaseAPIKey,
                     {
                     email: authData.payload.email,
                     password: authData.payload.password,
@@ -32,15 +33,37 @@ export class AuthEffects {
                     }
             ).pipe(map(resData => {
                 const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-                of(new AuthActions.Login({email: resData.email, userId: resData.localId, token: resData.idToken, expirationDate: expirationDate}));
-            }),catchError(error => {
-                return of();
+                return new AuthActions.Login({
+                        email: resData.email, 
+                        userId: resData.localId, 
+                        token: resData.idToken, 
+                        expirationDate: expirationDate});
+            }),catchError(errorRes => {
+                let errorMessage = 'An unknown error occurred!';
+                if (!errorRes.error || !errorRes.error.error) {
+                    return of(new AuthActions.LoginFail(errorMessage));
+                }
+                switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'This email exists already';
+                    break;
+                case 'EMAIL_NOT_FOUND':
+                    errorMessage = 'This email does not exist.';
+                    break;
+                case 'INVALID_PASSWORD':
+                    errorMessage = 'This password is not correct.';
+                    break;
+                }
+                return of(new AuthActions.LoginFail(errorMessage));
             })
             );
         })
     );
 
-    constructor(private actions$: Actions, private http: HttpClient) {
+    @Effect({dispatch: false})
+    authSuccess = this.actions$.pipe(ofType(AuthActions.LOGIN), tap(() => {this.router.navigate(['/']);}));
+
+    constructor(private actions$: Actions, private http: HttpClient, private router: Router) {
 
     }
 }
