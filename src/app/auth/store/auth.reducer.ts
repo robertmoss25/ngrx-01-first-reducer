fc0dc4716e72f3615ff44377d2ed19770c8fbc3f
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2020-02-17 15:17:47 -0500

    Managing UIState in NgRx

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -3,10 +3,14 @@ import * as AuthActions from './auth.actions';
 
 export interface State {
     user: User;
+    authError: string;
+    loading: boolean;
 }
 
 const initialState = {
-    user: null
+    user: null,
+    authError: null,
+    loading: false
 };
 
 export function authReducer(state = initialState, action: AuthActions.AuthActions) {
@@ -15,12 +19,27 @@ export function authReducer(state = initialState, action: AuthActions.AuthAction
             const tempUser = new User(action.payload.email, action.payload.userId,action.payload.token,action.payload.expirationDate);
             return {
                 ...state,
-                user: tempUser
+                user: tempUser,
+                authError: null,
+                loading: false
             };
         case AuthActions.LOGOUT:
             return {
                 ...state,
                 user: null
+            };
+        case AuthActions.LOGIN_START:
+            return {
+                ...state, 
+                authError: null,
+                loading: true
+            };
+        case AuthActions.LOGIN_FAIL:
+            return {
+                ...state, 
+                user: null,
+                authError: action.payload,
+                loading: false
             };
         default:
             return state;
