6cc495203af694cf903a2cc6b578b5e9d4d4a6b0
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2020-02-17 09:34:39 -0500

    Multiple Actions

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -18,7 +18,7 @@ export class DataStorageService {
     const recipes = this.recipeService.getRecipes();
     this.http
       .put(
-        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json',
+        'https://ng-course-recipe-book-83263.firebaseio.com/recipes.json',
         recipes
       )
       .subscribe(response => {
@@ -29,7 +29,7 @@ export class DataStorageService {
   fetchRecipes() {
     return this.http
       .get<Recipe[]>(
-        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
+        'https://ng-course-recipe-book-83263.firebaseio.com/recipes.json'
       )
       .pipe(
         map(recipes => {
