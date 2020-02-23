ea7d36877b252b71fbf3603672e7b6b0cdb9ef2b
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2020-02-23 12:34:33 -0500

    Finishing the Auth Effects

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,16 +3,6 @@ import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
 import { Store } from '@ngrx/store';
 
-export interface AuthResponseData {
-  kind: string;
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiresIn: string;
-  localId: string;
-  registered?: boolean;
-}
-
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private tokenExpirationTimer: any;
